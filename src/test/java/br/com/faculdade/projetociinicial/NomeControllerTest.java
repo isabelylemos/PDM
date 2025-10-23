a41ecde285e296d42981509d0795661dcf35f2bd
Isabely Lemos <160620081+isabelylemos@users.noreply.github.com>
2025-10-23 20:10:54 -0300

    test: actions

--- src/test/java/br/com/faculdade/projetociinicial/NomeControllerTest.java
+++ src/test/java/br/com/faculdade/projetociinicial/NomeControllerTest.java
@@ -1,7 +1,6 @@
 package br.com.faculdade.projetociinicial;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
